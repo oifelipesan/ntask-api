1a9b7affb4dc6d9415a953c5b008f856445b859a
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-10-30 18:35:41 -0300

    configure now.json and database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   development: {
     database: 'ntask',
@@ -10,10 +12,10 @@ module.exports = {
     }
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     operatorsAliases: false
   },
