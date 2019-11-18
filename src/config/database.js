9960e784915c0a742bca79d55ad04f3ab7400077
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-11-18 14:38:19 -0300

    Updating test configs

--- src/config/database.js
+++ src/config/database.js
@@ -1,30 +1,33 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
-    database: 'ntask',
-    username: '',
-    password: '',
-    dialect: 'sqlite',
-    storage: 'ntasks.sqlite',
+    database: "ntask",
+    username: "",
+    password: "",
+    dialect: "sqlite",
+    storage: "ntasks.sqlite",
     define: {
       undersored: true
     }
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    operatorsAliases: false
+    database: "ntask_test",
+    username: "",
+    password: "",
+    dialect: "sqlite",
+    storage: "ntasks.sqlite",
+    logging: false,
+    define: {
+      undersored: true
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: "mysql",
     operatorsAliases: false
   }
-}
+};
