83e13cccc6c63bcef059e035c5949975c69f319c
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-11-18 18:06:16 -0300

    Export App

--- src/server.js
+++ src/server.js
@@ -1,30 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const auth = require('./app/middlewares/auth')
-const cors = require('cors')
+const express = require("express")
+const bodyParser = require("body-parser")
+const helmet = require("helmet")
+const auth = require("./app/middlewares/auth")
+const cors = require("cors")
 
-const { port } = require('./config')
+const { port } = require("./config")
 const app = express()
 
-const routerUser = require('./routes/user')
-const routerTask = require('./routes/task')
-const routerAuth = require('./routes/auth')
+const routerUser = require("./routes/user")
+const routerTask = require("./routes/task")
+const routerAuth = require("./routes/auth")
 
 app.use(helmet())
 
-app.disable('x-powered-by')
+app.disable("x-powered-by")
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(auth.initialize())
 
-app.use('/users', routerUser)
-app.use('/tasks', routerTask)
-app.use('/auth', routerAuth)
+app.use("/users", routerUser)
+app.use("/tasks", routerTask)
+app.use("/auth", routerAuth)
 
 app.listen(port, err => {
-  if (err) console.log('Error')
+  if (err) console.log("Error")
   else console.log(`Server in running on port ${port}`)
 })
+
+module.exports = app
