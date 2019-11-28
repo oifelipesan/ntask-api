7777c2a2daa706e809d9cffacfa8c7af601bb5a8
Felipe dos Santos <felipesan.cwb@gmail.com>
2019-11-28 14:58:27 -0300

    Name changed

--- src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  return res.status(200).json({ status: 'Ntask API' })
-})
-
-module.exports = router
